e2ec09e89ebd3d726a38e91c40bc7994980d8525
Vaaarna <rasa.putnina@gmail.com>
2023-03-14 21:11:32 +0200

    horrible error killed me, rip

--- functions.js
+++ functions.js
@@ -1,6 +1,49 @@
+function getWeather(){
 
-document.getElementById("butt1").onclick = function() {getWether()};
+    let request = new XMLHttpRequest();
+    const url = `http://example.com/movies.json`;
 
-function getWether() {
-    document.getElementById("weatherOutput").innerHTML = "YOU CLICKED ME!";
-}
+    request.onreadystatechange = function() {
+      if (this.readyState === 4 && this.status === 200) {
+        const response = JSON.parse(this.responseText);
+        getElements(response);
+      }
+    };
+
+    request.open("GET", url, true);
+    request.send();
+
+   function getElements(response) {
+    //   $('.showHumidity').text(`The humidity in is ${response.main.humidity}%`);
+    //   $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
+    }
+
+  };
+
+
+// const userAction = async () => {
+//     const response = await fetch('http://example.com/movies.json');
+//     const myJson = await response.json(); //extract JSON from the http response
+//     // do something with myJson
+//   }
+
+ document.getElementById("butt1").onclick = function() {getWeather()};
+
+
+// const getWether = await() => { 
+//     console.log("testlog1");
+//     document.getElementById("weatherOutput").innerHTML = "YOU CLICKED ME!";
+
+//     const response =  fetch('https://api.open-meteo.com/v1/forecast?latitude=56.95&longitude=24.11&hourly=temperature_2m,windspeed_10m&forecast_days=1');
+//    console.log("testlog response");
+//     const myJson =  response.json(); //extract JSON from the http response
+//     console.log("testlog response");
+
+
+
+//     var wind_speed = 22.22;
+//     var temperatuer = 25.22;
+
+//     // Wind speed is 22 m/s and tempoeraturs is 25 C. 
+// }
+
