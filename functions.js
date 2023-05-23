12d3c41cc88b877ca6c793cd618989bc21dbcfe7
Vaaarna <rasa.putnina@gmail.com>
2023-05-23 22:58:01 +0300

    i am now v seepy, did good job :)

--- functions.js
+++ functions.js
@@ -1,18 +1,39 @@
-async function getWeather(){
+
+class OneHour {
+  constructor(time, temperature, wimd, sunny) {
+    this.time = time;
+    this.temperature = temperature;
+    this.wind = wimd;
+
+  }
+}
+
+async function getWeather() {
 
   async function getData(url) {
     const response = await fetch(url);
     return response.json();
   }
-  
+
   const url = `https://api.open-meteo.com/v1/forecast?latitude=56.95&longitude=24.11&hourly=temperature_2m,windspeed_10m&forecast_days=1`;
   const data = await getData(url);
-  
+
   console.log({ data });
-  console.log(data["elevation"]);
 
-  };
+  const stundinas = []
 
- document.getElementById("butt1").onclick = function() {getWeather()};
+  for (let i = 0; i < 23; i++) {
+
+    let normDate = new Date(data["hourly"]["time"][i])
+    let hour = new OneHour(normDate, data["hourly"]["temperature_2m"][i], data["hourly"]["windspeed_10m"][i], 3)
+
+    stundinas.push(hour)
+
+  }
+  console.log(stundinas)
+
+};
+
+document.getElementById("butt1").onclick = function () { getWeather() };
 
 
