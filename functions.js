8ed1fae4170f57f845d932413402531fff68fbe7
Vaaarna <rasa.putnina@gmail.com>
2023-05-24 10:35:08 +0300

    minor edits + comments

--- functions.js
+++ functions.js
@@ -1,5 +1,5 @@
 
-class OneHour {
+class oneHourData {
   constructor(time, temperature, wimd, sunny) {
     this.time = time;
     this.temperature = temperature;
@@ -9,31 +9,33 @@ class OneHour {
 }
 
 async function getWeather() {
-
+// dabut datus no magic interneta DONT TOUCH
   async function getData(url) {
     const response = await fetch(url);
     return response.json();
   }
-
+// okai
   const url = `https://api.open-meteo.com/v1/forecast?latitude=56.95&longitude=24.11&hourly=temperature_2m,windspeed_10m&forecast_days=1`;
   const data = await getData(url);
 
   console.log({ data });
 
-  const stundinas = []
+  const oneHourArr = []
 
   for (let i = 0; i < 23; i++) {
 
-    let normDate = new Date(data["hourly"]["time"][i])
-    let hour = new OneHour(normDate, data["hourly"]["temperature_2m"][i], data["hourly"]["windspeed_10m"][i], 3)
+    let normDate = new Date(data["hourly"]["time"][i]) //turn weird date into uniform simple date
+    let oneHourData = new oneHourData(normDate, data["hourly"]["temperature_2m"][i], data["hourly"]["windspeed_10m"][i] //salikt stundai klat atbilstosos datus
 
-    stundinas.push(hour)
+    oneHourArr.push(oneHourData)
 
   }
-  console.log(stundinas)
+  console.log(oneHourArr)
 
 };
 
 document.getElementById("butt1").onclick = function () { getWeather() };
+
+
 
 
